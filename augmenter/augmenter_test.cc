c9d6a3b7d1020b51812eb7850432e9da2d83a31c
Christopher Brix <brix@google.com>
2020-09-22 11:55:28 +0000

    Added tests for phone replacement

--- augmenter/augmenter_test.cc
+++ augmenter/augmenter_test.cc
@@ -27,6 +27,10 @@
 
 namespace augmenter {
 
+using ::testing::InSequence;
+using ::testing::Return;
+using ::testing::ReturnRef;
+
 struct TokenSpec {
   const std::string& text;
   const int start;
@@ -100,6 +104,30 @@ bert_annotator::Documents ConstructBertDocument(
   }
 
   return documents;
+}
+
+void ExpectEq(bert_annotator::Document a, bert_annotator::Document b) {
+  EXPECT_STREQ(a.text().c_str(), b.text().c_str());
+  EXPECT_EQ(a.token_size(), b.token_size());
+  for (int i = 0; i < a.token_size(); ++i) {
+    EXPECT_STREQ(a.token(i).word().c_str(), b.token(i).word().c_str());
+    EXPECT_EQ(a.token(i).start(), b.token(i).start());
+    EXPECT_EQ(a.token(i).end(), b.token(i).end());
+  }
+  EXPECT_EQ(a.labeled_spans_size(), b.labeled_spans_size());
+  for (auto map_entry : a.labeled_spans()) {
+    auto key = map_entry.first;
+    EXPECT_EQ(a.labeled_spans().at(key).labeled_span_size(),
+              b.labeled_spans().at(key).labeled_span_size());
+    for (int i = 0; i < a.labeled_spans().at(key).labeled_span_size(); ++i) {
+      EXPECT_STREQ(a.labeled_spans().at(key).labeled_span(i).label().c_str(),
+                   b.labeled_spans().at(key).labeled_span(i).label().c_str());
+      EXPECT_EQ(a.labeled_spans().at(key).labeled_span(i).token_start(),
+                b.labeled_spans().at(key).labeled_span(i).token_start());
+      EXPECT_EQ(a.labeled_spans().at(key).labeled_span(i).token_end(),
+                b.labeled_spans().at(key).labeled_span(i).token_end());
+    }
+  }
 }
 
 TEST(AugmenterTest, NoAugmentation) {
@@ -201,23 +229,23 @@ TEST(AugmenterTest, RandomizedLowercasing) {
   absl::MockingBitGen bitgen;
   // Document sampling.
   EXPECT_CALL(absl::MockUniform<int>(), Call(bitgen, 0, 2))
-      .WillOnce(testing::Return(0))
-      .WillOnce(testing::Return(1))
-      .WillOnce(testing::Return(2))
-      .WillOnce(testing::Return(0));
+      .WillOnce(Return(0))
+      .WillOnce(Return(1))
+      .WillOnce(Return(2))
+      .WillOnce(Return(0));
   // Phone/Address replacement likelihood.
   EXPECT_CALL(absl::MockBernoulli(), Call(bitgen, 0))
       .WillRepeatedly(testing::Return(false));
   // Lowercasing.
-  testing::InSequence s;
+  InSequence s;
   EXPECT_CALL(absl::MockBernoulli(), Call(bitgen, 2.0 / 4))
-      .WillOnce(testing::Return(true));
+      .WillOnce(Return(true));
   EXPECT_CALL(absl::MockBernoulli(), Call(bitgen, 1.0 / 3))
-      .WillOnce(testing::Return(false));
+      .WillOnce(Return(false));
   EXPECT_CALL(absl::MockBernoulli(), Call(bitgen, 1.0 / 2))
-      .WillOnce(testing::Return(false));
+      .WillOnce(Return(false));
   EXPECT_CALL(absl::MockBernoulli(), Call(bitgen, 1.0 / 1))
-      .WillOnce(testing::Return(true));
+      .WillOnce(Return(true));
   MockRandomSampler address_sampler;
   MockRandomSampler phones_sampler;
   Augmenter augmenter =
@@ -269,6 +297,39 @@ TEST(AugmenterTest, DontReplacePhone) {
   EXPECT_EQ(augmenter.documents().documents_size(), 2);
   EXPECT_STREQ(augmenter.documents().documents(1).text().c_str(),
                "Call 0123456789!");
+}
+
+TEST(AugmenterTest, ReplacePhoneSameLength) {
+  bert_annotator::Documents documents = ConstructBertDocument(
+      {DocumentSpec("Call 0123456789!",
+                    {TokenSpec("Call", 0, 3), TokenSpec("0123456789", 5, 14)},
+                    {{"lucid", {LabelSpec("TELEPHONE", 1, 1)}}})});
+  MockRandomSampler address_sampler;
+  MockRandomSampler phones_sampler;
+  std::string replacement = "9876543210";
+  EXPECT_CALL(phones_sampler, Sample()).WillOnce(ReturnRef(replacement));
+  // Address/Phone replacement likelihood.
+  absl::MockingBitGen bitgen;
+  EXPECT_CALL(absl::MockBernoulli(), Call(bitgen, 0))
+      .WillOnce(testing::Return(false));
+  EXPECT_CALL(absl::MockBernoulli(), Call(bitgen, 1))
+      .WillOnce(testing::Return(true));
+
+  Augmenter augmenter =
+      Augmenter(documents, &address_sampler, &phones_sampler, bitgen);
+
+  augmenter.Augment(/*total=*/1, /*lowercase=*/0, /*addresses=*/0,
+                    /*phones=*/1);
+
+  bert_annotator::Document augmented = augmenter.documents().documents(1);
+  bert_annotator::Document expected =
+      ConstructBertDocument(
+          {DocumentSpec(
+              "Call 9876543210!",
+              {TokenSpec("Call", 0, 3), TokenSpec("9876543210", 5, 14)},
+              {{"lucid", {LabelSpec("TELEPHONE", 1, 1)}}})})
+          .documents(0);
+  ExpectEq(augmented, expected);
 }
 
 }  // namespace augmenter
