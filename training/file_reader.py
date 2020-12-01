974dfc9cb933227aafb1178e969d8fb4af19d94b
Christopher Brix <brix@google.com>
2020-12-01 13:52:47 +0000

    Fix for the merging of sentences in .lftxt files: leading/trailing whitespace should not prevent sentences from being merged.

--- training/file_reader.py
+++ training/file_reader.py
@@ -313,6 +313,7 @@ class LftxtReader(FileReader):
 
                 text_without_braces = text.replace("{{{",
                                                    "").replace("}}}", "")
+                text_without_braces = text_without_braces.strip()
 
                 yield LabeledExample(prefix=prefix,
                                      selection=labeled_text,
